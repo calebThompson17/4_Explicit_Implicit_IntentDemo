6c2f35b9a8d3b9435c6531e72ebdcecd69a092cf
Keith <kperkins411@gmail.com>
2020-01-30 16:02:06 -0500

    lect

--- app/src/main/java/com/example/intentdemo/IntentActivity.java
+++ app/src/main/java/com/example/intentdemo/IntentActivity.java
@@ -11,12 +11,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
